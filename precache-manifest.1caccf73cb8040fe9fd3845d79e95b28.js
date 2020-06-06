17f1530ac53f8ad97447dd830d2a3bcca52f98e5
ponka123 <anka.irhina@gmail.com>
2020-06-06 09:18:29 -0700

    Updates

---
+++ precache-manifest.1caccf73cb8040fe9fd3845d79e95b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef12ddc95716cf27953bc5703df60824",
+    "url": "/kanban2/index.html"
+  },
+  {
+    "revision": "127ce63af4c9c795b784",
+    "url": "/kanban2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "127ce63af4c9c795b784",
+    "url": "/kanban2/static/js/2.dd60000a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban2/static/js/2.dd60000a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eaac64bef779893605d",
+    "url": "/kanban2/static/js/main.b12bf8dc.chunk.js"
+  },
+  {
+    "revision": "db288dc7a0c0720f1967",
+    "url": "/kanban2/static/js/runtime-main.09a05c5e.js"
+  }
+]);
